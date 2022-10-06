5ae23170f940c6226f2443b042f903d0a291b54e
Hayden Center <haydencenter72@gmail.com>
2022-10-06 17:48:28 -0500

    EC2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,9 +9,9 @@ function App () {
   const [values, setValues] = useState([])
 
   // ENTER YOUR EC2 PUBLIC IP/URL HERE
-  const ec2_url = ''
+  const ec2_url = 'ec2-54-157-231-62.compute-1.amazonaws.com'
   // CHANGE THIS TO TRUE IF HOSTING ON EC2, MAKE SURE TO ADD IP/URL ABOVE
-  const ec2 = false;
+  const ec2 = true;
   // USE localhost OR ec2_url ACCORDING TO ENVIRONMENT
   const url = ec2 ? ec2_url : 'localhost'
 
