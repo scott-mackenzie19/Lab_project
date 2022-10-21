eeebb0b514905edb9e3be0cb41bd9301ac9d9e57
vkatvell <vvellanki@smu.edu>
2022-10-20 20:13:52 -0500

    Added EC2 URL

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,15 @@ import './App.css';
 import axios from 'axios';
 
 // React functional component
-function App () {
+function App() {
   // state for storage of the information on the webpage of forms and list, uses hooks
   const [number, setNumber] = useState("")
   const [values, setValues] = useState([])
 
   // ENTER YOUR EC2 PUBLIC IP/URL HERE
-  const ec2_url = ''
+  const ec2_url = 'ec2-52-14-129-198.us-east-2.compute.amazonaws.com'
   // CHANGE THIS TO TRUE IF HOSTING ON EC2, MAKE SURE TO ADD IP/URL ABOVE
-  const ec2 = false;
+  const ec2 = true;
   // USE localhost OR ec2_url ACCORDING TO ENVIRONMENT
   const url = ec2 ? ec2_url : 'localhost'
 
@@ -21,7 +21,7 @@ function App () {
   }
 
   const fetchBase = () => {
-    axios.get(`http://${url}:8000/`).then((res)=>{
+    axios.get(`http://${url}:8000/`).then((res) => {
       alert(res.data);
     })
   }
@@ -33,16 +33,16 @@ function App () {
         const values = res.data.data;
         console.log(values);
         setValues(values)
-    }).catch(err => {
-      console.log(err)
-    });
+      }).catch(err => {
+        console.log(err)
+      });
   }
 
   // handle input form submission to backend via POST request
   const handleSubmit = (e) => {
     e.preventDefault();
     let prod = number * number;
-    axios.post(`http://${url}:8000/multplynumber`, {product: prod}).then(res => {
+    axios.post(`http://${url}:8000/multplynumber`, { product: prod }).then(res => {
       console.log(res);
       fetchVals();
     }).catch(err => {
@@ -65,21 +65,21 @@ function App () {
   // the comment below silences an error that doesn't matter.=
   useEffect(() => {
     fetchVals();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={fetchBase} style={{marginBottom: '1rem'}}> {`GET: http://${url}:8000/`} </button>
+        <button onClick={fetchBase} style={{ marginBottom: '1rem' }}> {`GET: http://${url}:8000/`} </button>
         <button onClick={reset}> Reset DB </button>
         <form onSubmit={handleSubmit}>
-          <input type="text" value={number} onChange={handleChange}/>
-          <br/>
+          <input type="text" value={number} onChange={handleChange} />
+          <br />
           <input type="submit" value="Submit" />
         </form>
         <ul>
-          { values.map((value, i) => <li key={i}>{value.value}</li>) }
+          {values.map((value, i) => <li key={i}>{value.value}</li>)}
         </ul>
       </header>
     </div>
