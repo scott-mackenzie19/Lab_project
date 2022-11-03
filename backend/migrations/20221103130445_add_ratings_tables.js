d48baf7787590611d6817b6f3a3c831199d6e6cb
nhon14 <92124287+nhon14@users.noreply.github.com>
2022-11-03 08:09:10 -0500

    added migrations for likes and ratings

---
+++ backend/migrations/20221103130445_add_ratings_tables.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+
+    return knex.schema.createTable('ratings', (table) => {
+        table.string('userID');
+        table.string('ratedID');
+        table.integer('rating');
+    });
+
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+
+    return knex.schema.dropTable('ratings');
+
+};
