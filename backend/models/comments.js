36ff74dadfaf49768d0ba868528162b9897fb9ca
nhon14 <92124287+nhon14@users.noreply.github.com>
2022-11-10 10:53:47 -0600

    made changes to comments model

--- backend/models/comments.js
+++ backend/models/comments.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 const LIKES_TABLE = 'likes';
 
 // get comments and display on page
-const fetchAllComments = async (comment, userID, eventID) => {
+const fetchAllComments = async (comment, userID/*, eventID*/) => {
     const query = knex(COMMENTS_TABLE);
     const results = await query;
 
@@ -17,12 +17,13 @@ const fetchAllComments = async (comment, userID, eventID) => {
 // }
 
 // handle post request and store comment 
-const createComment = async (comment, userID, eventID) => {
-    const query = knex(COMMENTS_TABLE).insert({name});
+const createComment = async (comment, userID/*, eventID*/) => {
+    const query = knex(COMMENTS_TABLE).insert({comment});
     const results = await query;
     return results;
 }
 
+// might not need
 const deleteComment = async (userID, eventID) => {
     const query = knex(COMMENTS_TABLE).delete().where({userID, eventID});
     const results = await query;
