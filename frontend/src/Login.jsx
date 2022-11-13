b408bd945a78e45effc8cad072bf0d4c6e485fef
scott-mackenzie-19 <samackenzie@smu.edu>
2022-11-12 18:38:01 -0600

    front end almost connects with backend maybe

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -6,6 +6,10 @@ import  Form  from "react-bootstrap/Form";
 
 import Button from "react-bootstrap/Button";
 
+import axios from "axios";
+axios.defaults.baseURL='http://ec2-52-14-129-198.us-east-2.compute.amazonaws.com/'
+
+
 export default function Login() {
     const [username, setUsername] = useState("");
   
@@ -22,17 +26,33 @@ export default function Login() {
     function handleSubmit(event) {
       event.preventDefault();
     }
+    
+
+    let body = {usernname:username, password:password};
+    const postUser = () => new Promise((resolve, reject) => {
+      let body = {username:username, password:password};
+      console.log(body)
+      console.log(axios.defaults.baseURL);
+      axios.post(`${axios.defaults.baseURL}`, body)
+      .then(x =>resolve(x.data))
+      .catch(x => {
+          alert(x);
+          reject(x);
+      })
+  });
+
+    
   
     return (
       <div className="Login">
-        <h2 class="a"> Website Title </h2>
-        <Form onSubmit={handleSubmit}>
-          <Form.Group size="lg" controlId="username" class="c">
-            <Form.Label class="c">Username</Form.Label>
+        <h2 className="a"> Website Title </h2>
+        <form id='form' onSubmit={handleSubmit}>
+          <Form.Group size="lg" controlId="username" className="c">
+            <Form.Label className="c">Username</Form.Label>
   
             <Form.Control
               autoFocus
-              class="b"
+              className="b"
               type="username"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
@@ -40,28 +60,29 @@ export default function Login() {
             />
           </Form.Group>
   
-          <Form.Group size="lg" controlId="password" class="c">
-            <Form.Label class="c">Password</Form.Label>
+          <Form.Group size="lg" controlId="password" className="c">
+            <Form.Label className="c">Password</Form.Label>
   
             <Form.Control
               type="password"
-              class="b"
+              className="b"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               //password set to current value in box
             />
           </Form.Group>
   
-          <Button block size="lg" type="submit" disabled={!validateForm()}>
+          <Button onClick= {postUser} block size="lg" type="submit" disabled={!validateForm()}>
             Login
           </Button>
   
           <hr />
   
-          <h2 class="d">or</h2>
+          <h2 className="d">or</h2>
   
-          <h2 class="e">Don’t have an account? Sign up now.</h2>
-        </Form>
+          <h2 className="e">Don’t have an account? Sign up now.</h2>
+        </form>
+
       </div>
     );
   }
